b439397e2201f645dfe304e738011bbcde29456c
Douglas <douglasd3v.contato@gmail.com>
2026-05-23 18:27:27 -0300

    Style(app.js): mudando nome da variavel de rotas de auth

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const app = express();
 
 const connection = require("./src/config/database.js");
-const userRoutes = require("./src/routes/auth.routes.js");
+const authRoutes = require("./src/routes/auth.routes.js");
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 connection().then(() => {
   app.listen(port, () => {
