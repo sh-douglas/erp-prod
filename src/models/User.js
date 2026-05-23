640444c6ac05b654964cfef738143e76f2503b9b
Douglas <douglasd3v.contato@gmail.com>
2026-05-23 12:43:49 -0300

    Refactor(User.js): Movendo schema para outro arquivo, mantendo a estrutura limpa

--- src/models/User.js
+++ src/models/User.js
@@ -1,33 +1,5 @@
 const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    passwordHash: {
-      type: String,
-      required: true,
-    },
-    employeeCode: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+const UserSchema = require("../schemas/user.schema.js");
 
 const User = mongoose.model("User", UserSchema);
 
