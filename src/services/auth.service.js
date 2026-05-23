6b2d8035fd8f0d20b4b9a873c78a0a8fe995df33
Douglas <douglasd3v.contato@gmail.com>
2026-05-23 18:26:39 -0300

    Feat(auth.service.js): Levando lógica de login/recuperação de senha para um service, facilitando manutenção

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,14 @@
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const AppError = require("../utils/AppError.js");
 const UserRepository = require("../repositories/user.repository.js");
 const counterService = require("../services/counter.service.js");
-const { registerSchema } = require("../validators/auth.validator.js");
+const {
+  registerSchema,
+  loginSchema,
+  passwordRecoverySchema,
+} = require("../validators/auth.validator.js");
 
 async function register(data) {
   const cleanData = registerSchema.parse(data);
@@ -32,6 +38,67 @@ async function register(data) {
   };
 }
 
+async function login(data) {
+  const cleanData = loginSchema.parse(data);
+  const registeredUser = await UserRepository.findByEmployeeCode(
+    cleanData.employeeCode,
+  );
+
+  if (!registeredUser) {
+    throw new AppError("Código ou senha inválidos.", 401);
+  }
+
+  const passwordMatch = await bcryptjs.compare(
+    cleanData.password,
+    registeredUser.passwordHash,
+  );
+
+  if (!passwordMatch) {
+    throw new AppError("Código ou senha inválidos.", 401);
+  }
+
+  const token = jwt.sign(
+    {
+      sub: registeredUser._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  return {
+    message: "Login realizado com sucesso!",
+    token,
+    user: {
+      id: registeredUser._id,
+      name: registeredUser.name,
+      email: registeredUser.email,
+      employeeCode: registeredUser.employeeCode,
+    },
+  };
+}
+
+async function passwordRecovery(data) {
+  const cleanData = passwordRecoverySchema.parse(data);
+  const registeredUser = await UserRepository.findByEmployeeCode(
+    cleanData.employeeCode,
+  );
+
+  if (!registeredUser) {
+    throw new AppError("Código de usuário não encontrado.", 404);
+  }
+
+  const salt = await bcryptjs.genSalt(12);
+  const hashedNewPassword = await bcryptjs.hash(cleanData.newPassword, salt);
+
+  await UserRepository.updatePassword(registeredUser, hashedNewPassword);
+
+  return {
+    message: "Senha alterada com sucesso!",
+  };
+}
+
 module.exports = {
   register,
+  login,
+  passwordRecovery,
 };
